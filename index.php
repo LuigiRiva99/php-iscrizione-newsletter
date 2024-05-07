f1a48e0a0942e511f9290a74895ddbe6a2e2c823
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-05-07 14:13:22 +0200

    ADD: form and alerts based on user input

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+//recupero mail dall'input
+$email = $_POST['email'] ?? null;
+
+
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +15,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    
+    <form action="" method="POST">
+        <input type="email" name="email" placeholder="Inserisci la tua email">
+        <button type="submit" class="btn btn-primary">Invia</button>
+        <?php
+            //controllo se $email contiene @ e .
+            if ($email) {
+                if (str_contains($email,'@') && str_contains($email,'.')) {
+                    ?>
+                        <div class="alert alert-success" role="alert">
+                            E-mail inserita correttamente
+                        </div>
+                    <?php
+                }else {
+                    ?>
+                        <div class="alert alert-danger" role="alert">
+                            E-mail non valida
+                        </div>
+                    <?php
+                }
+            }
+        ?>
+
+    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
 </body>
 </html>
